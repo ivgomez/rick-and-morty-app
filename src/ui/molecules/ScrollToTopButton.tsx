46897eddc450ae69782d2e8984ac140a6e5d6852
ivgomez <iv.gomez@hotmail.com>
2024-06-16 11:47:21 -0500

    code refactor for modal component

--- src/ui/molecules/ScrollToTopButton.tsx
+++ src/ui/molecules/ScrollToTopButton.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
-import {TouchableOpacity, StyleSheet, Animated} from 'react-native';
+import React, {useEffect} from 'react';
+import {TouchableOpacity, StyleSheet} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+} from 'react-native-reanimated';
 
 type Props = {
   onPress: () => void;
@@ -8,8 +13,18 @@ type Props = {
 };
 
 const ScrollToTopButton: React.FC<Props> = ({onPress, visible}) => {
+  const opacity = useSharedValue(0);
+
+  useEffect(() => {
+    opacity.value = withTiming(visible ? 1 : 0, {duration: 300});
+  }, [visible, opacity]);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    opacity: opacity.value,
+  }));
+
   return (
-    <Animated.View style={[styles.container, {opacity: visible ? 1 : 0}]}>
+    <Animated.View style={[styles.container, animatedStyle]}>
       <TouchableOpacity style={styles.button} onPress={onPress}>
         <Icon name="arrow-up" size={25} color="#fff" />
       </TouchableOpacity>
